356c9b63b9b86ca3e3904a9e19b7c748e8163a24
lpg <1003289700@qq.com>
2017-07-18 23:18:26 +0800

    修改

--- src/page/product/index.vue
+++ src/page/product/index.vue
@@ -1,5 +1,5 @@
     <template>
-    <div style="padding-top: 1rem; background: #F6F7F9;">
+    <div style="padding-top: 1rem; padding-bottom: 1rem; background: #F6F7F9; min-height: calc(100vh - 1rem);">
         <head-i><span class="head_title" slot="title_text">我的产品录入</span><router-link class="head_a" slot="head_a" to="/product/info">介绍</router-link></head-i>
         <div class="from">
             <p class="pt"><span class="xing">*</span>产品信息</p>
@@ -7,11 +7,25 @@
             <div class="select" @click="tankan('pinpai')">品牌<span class="nr">{{pinpai}}</span></div>
         </div>
         <div class="from">
-            <div :key="data" v-for="data,key in properties" class="bor_b" :class="data.show_type=='select'?'select':'text'">
+            <div :key="data" v-for="data,key in propertiesData" class="bor_b" :class="data.show_type=='text'?'text':'select'">
                 <span class="sp" :class="data.name.length>4?'lk':''">{{data.name}}</span>
-                <input type="text" value="" v-model="data.id" placeholder="请选择">
+                <input type="text" value="" placeholder="请输入" v-if="data.show_type=='text'">
+                <input type="date" value="" placeholder="请选择" v-else-if="data.show_type=='date'">
+                <select v-else="data.show_type=='text'">
+                    <option>请选择</option>
+                    <option v-for="data in fenge(data.values)">{{data}}</option>
+                </select>
             </div>
         </div>
+        <div class="from" style="margin-bottom: 1px;">
+            <span class="xing1">*</span><p class="p_up">附件上传（购买发票、商品图片、保修单等）支持单个大小不超过2M，格式为.jpg、.jpeg、.png、.bmp格式。消费保将对您的产品严格保密，请放心上传。</p>
+            <div id="uploader">
+                <div id="fileList" class="uploader-list"></div>
+                <div style="clear: both;"></div>
+            </div>
+            <div class="up_info"><img src="../../images/icon_tishi@2x.png">产品录入后可以直接与企业售后平台联系或者投诉产品</div>
+        </div>
+        <div class="form_but">保存</div>
         <div v-if="tk" class="tk" @click="qx"></div>
         <div v-if="tkbox" class="tkbox">
             <div class="title">
@@ -50,18 +64,21 @@
     export default {
         data(){
             return {
+                tjid1:'',
+                tjid2:'',
                 type_id:'',
                 type_name:'',
                 subtype_id:'',
                 subtype_name:'',
                 brand_id:'',
                 brand_name:'',
-                properties:'',
+                properties:{},
                 fenlei:'请选择',
                 pinpai:'请选择',
                 tkData:'',
                 brandData:'',
                 typeData:'',
+                propertiesData:'',
                 tk:false,
                 tkbox:false,
                 showLoad:false,
@@ -81,6 +98,9 @@
             this.ajaxData();
         },
         methods:{
+            fenge(i){
+                return i.split("|||");
+            },
             ajaxData(){
                 this.showLoad=true;
                 this.loadType='load';
@@ -116,9 +136,10 @@
                 this.loadText='正在加载';
                 this.axios.get('/v4/complaint/properties?subtype_id='+i)
                     .then(res => {
-                        this.properties=res.data.data;
+                        this.propertiesData=res.data.data;
                         this.close();
                     })
+
                     .catch(err => {
                         console.log(err);
                     })
@@ -142,10 +163,14 @@
             but(){
                 if(this.tkData.name=='type'){
                     if(this.type_id!='' && this.subtype_id!=''){
+
                         this.fenlei=this.type_name+' '+this.subtype_name;
+                        this.tjid1=this.type_id;
+                        this.tjid2=this.subtype_id;
                         this.ajaxBrand(this.type_id);
                         this.ajaxFujia(this.type_id);
                         this.qx();
+                        console.log(this.tjid1,this.tjid2);
                     }else{
                         this.showLoad=true;
                         this.loadType='alert';
@@ -186,6 +211,10 @@
                 document.querySelector('body').style.overflow='hidden';
             },
             qx(){
+                if(this.tjid1!='' || this.tjid2!=''){
+                    this.type_id = this.tjid1;
+                    this.subtype_id = this.tjid2;
+                }
                 this.tk=false;
                 this.tkbox=false;
                 document.querySelector('body').style.overflow='';
@@ -263,18 +292,56 @@
             }
         }
     }
+    .form_but{
+        width: 100%;
+        background: #2dc176;
+        font-size: .4rem;
+        color: #fff;
+        height: .98rem;
+        line-height: .98rem;
+        text-align: center;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+    }
     .tk{ width: 100%; height: 100%; z-index: 999; background: rgba(0,0,0,.5); position: fixed; left: 0; top: 0;}
-    .from{ margin-bottom:.2rem; background: #fff;}
+    .from{
+        margin-bottom:.2rem;
+        background: #fff;
+        .p_up{
+            font-size: .29rem;
+            color: #333;
+            padding: .2rem .2rem .3rem .5rem;
+        }
+        .up_info{
+            font-size: .26rem;
+            color: #999;
+            overflow: hidden;
+            line-height: .44rem;
+            padding-bottom: .2rem;
+            img{
+                width: .44rem;
+                height: .44rem;
+                float: left;
+                margin: 0 .2rem;
+            }
+        }
+    }
+    .from div:last-child{ border-bottom: none;}
     .bor_b{ border-bottom: 1px dashed rgba(204,204,204,1);}
-    .nr{float: right; color: #a6a6a6; margin-right: .4rem;}
+    .nr{float: right; color: #999; margin-right: .4rem; font-size: .28rem;}
     .select{ height: .85rem; line-height: .85rem; width: auto; margin: 0 .22rem; background:#fff url("../../images/icon_next@2x.png") no-repeat center right; background-size:.18rem auto; font-size: .32rem;}
-    .select .sp{float: left;  display: block;  width: 1.3rem;}
-    .text .sp{float: left;  display: block;  width: 1.3rem;}
+    .select .sp{float: left;  display: block;  width: 1.4rem;}
+    input[type=date]::-webkit-inner-spin-button { visibility: hidden; }
+    .text .sp{float: left;  display: block;  width: 1.4rem;}
+    .text input{ width: calc(~'100% - 1.7rem'); font-size: .28rem; height: .83rem; float: left; text-align: right; color: #999;}
+    .select input{ width: calc(~'100% - 1.7rem'); font-size: .28rem; height: .83rem; float: left; text-align: right; color: #999;}
+    .select select{ width: calc(~'100% - 1.7rem'); font-size: .28rem; height: .83rem; float: left; direction: rtl; color: #999; appearance:none; -moz-appearance:none;  -webkit-appearance:none;}
     .lk{line-height: .425rem;}
     .text{ height: .85rem; line-height: .85rem; width: auto; margin: 0 .22rem; background-size:.18rem auto; font-size: .32rem;}
     .pt{ height: .56rem; line-height: .56rem; background: #f6f7f9; font-size:.26rem; color: #b6b6b6;}
     .xing{ font-size: .4rem; height: .56rem; line-height: .66rem; float: left; color: #2dc176; margin: 0 .2rem 0 .2rem;}
-    .xing1{ font-size: .4rem; height: .4rem; line-height: .5rem; float: left; color: #2dc176; margin: 0 .1rem 0 .2rem;}
+    .xing1{ font-size: .4rem; height: .4rem; line-height: .5rem; float: left; color: #2dc176; margin: .2rem .1rem 0 .2rem;}
     .loading{
         width: 100%;
         height: .6rem;
